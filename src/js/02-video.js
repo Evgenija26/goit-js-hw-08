1ee6ab5119b2f4daddc87f8d98c92886f0d39d19
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2022-09-24 16:26:29 +0300

    hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const CURRENT_TIME_KEY = 'videoplayer-current-time';
+// const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-const refs = {
-  player: document.querySelector('#vimeo-player'),
-};
+// const refs = {
+//   player: document.querySelector('#vimeo-player'),
+// };
 
-const player = new Player(refs.player);
+// const player = new Player(refs.player);
 
-player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY) || 0);
-player.on('timeupdate', throttle(onTimeUpdate, 1000));
+// player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY) || 0);
+// player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
-function onTimeUpdate({ seconds }) {
-  localStorage.setItem(CURRENT_TIME_KEY, seconds);
-}
+// function onTimeUpdate({ seconds }) {
+//   localStorage.setItem(CURRENT_TIME_KEY, seconds);
+// }
