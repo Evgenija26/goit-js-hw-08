b49e9a33393f97469d3ba6b7ddbc34569a068353
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2022-09-24 21:30:30 +0300

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 const galleryRef = document.querySelector(".gallery");
 const listImg = galleryItems.map((item) => {
@@ -12,3 +14,4 @@ new SimpleLightbox(".gallery a", {
   captionsData: "alt",
 });
 console.log(galleryItems);
+
